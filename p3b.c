5b392137ba4e64630d8c56b5ef955cf5d4f0e658
ananyagowdakn <ananyagowdakn@gmail.com>
2025-11-19 07:59:58 +0530

    Implement swapping of two numbers in C

---
+++ p3b.c
@@ -0,0 +1,16 @@
+//C Program for swapping of two numbers.
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    scanf("%d %d", &a, &b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("%d %d", a, b);
+
+    return 0;
+}
