3c2c850a569f48f62c03723b67e8457b1c528556
ananyagowdakn <ananyagowdakn@gmail.com>
2025-11-19 08:06:23 +0530

    Add logical operator examples in p3e.c
    Implement logical operators demonstration in C

---
+++ p3e.c
@@ -0,0 +1,14 @@
+//logical operator
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("(a && b) : %d\n", a && b);
+    printf("(a || b) : %d\n", a || b);
+    printf("(!a)     : %d\n", !a);
+    printf("(!b)     : %d\n", !b);
+
+    return 0;
+}
