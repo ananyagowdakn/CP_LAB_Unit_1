1f47cc0f3c81d10cf2da1b393b6082a5888ba3c4
ananyagowdakn <ananyagowdakn@gmail.com>
2025-11-19 08:38:02 +0530

    Add factorial calculation program in p8.c
    Implement a program to calculate the factorial of a number.

---
+++ p8.c
@@ -0,0 +1,22 @@
+//Factorial of a number#include <stdio.h>
+
+int main() {
+    int n, i;
+    long long fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n < 0) {
+        printf("Factorial of a negative number doesn't exist.\n");
+        return 0;
+    }
+
+    for(i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+
+    printf("Factorial of %d = %lld\n", n, fact);
+
+    return 0;
+}
