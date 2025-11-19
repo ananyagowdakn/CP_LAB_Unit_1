dd925bdd70d4e54ca32e66e4b0ea931bcba97307
ananyagowdakn <ananyagowdakn@gmail.com>
2025-11-19 08:34:00 +0530

    Add C program for arithmetic operations
    Implement a C program for basic arithmetic operations.

---
+++ p5.c
@@ -0,0 +1,20 @@
+//C program to perform all Arithmetic Operations ( + , - , * , / )
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Addition = %d\n", a + b);
+    printf("Subtraction = %d\n", a - b);
+    printf("Multiplication = %d\n", a * b);
+
+    if(b != 0)
+        printf("Division = %.2f\n", (float)a / b);
+    else
+        printf("Division not possible (division by zero!)\n");
+
+    return 0;
+}
