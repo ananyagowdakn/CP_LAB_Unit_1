cb1347c1fa9367afeb894751ed27c58d90f33028
ananyagowdakn <ananyagowdakn@gmail.com>
2025-11-19 08:08:28 +0530

    Implement max function using conditional operator

---
+++ p3f.c
@@ -0,0 +1,12 @@
+//conditional operator
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int max = (a > b) ? a : b;
+    printf("%d\n", max);
+
+    return 0;
+}
