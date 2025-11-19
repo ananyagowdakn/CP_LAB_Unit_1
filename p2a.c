dffde1c2c23903db43d066157db6ddb6bbf016db
ananyagowdakn <ananyagowdakn@gmail.com>
2025-11-19 07:48:58 +0530

    Add C program to display various data types
    C program to display different data types including int, float, double, and char.

---
+++ p2a.c
@@ -0,0 +1,15 @@
+//C program to display different data types
+#include <stdio.h>
+int main() {
+    int a = 10;
+    float b = 12.5;
+    double c = 123.456;
+    char d = 'A';
+
+    printf("Integer value: %d\n", a);
+    printf("Float value: %f\n", b);
+    printf("Double value: %lf\n", c);
+    printf("Character value: %c\n", d);
+
+    return 0;
+}
