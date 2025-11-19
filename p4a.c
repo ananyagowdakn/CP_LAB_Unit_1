a6b0fb699d795f102130f133a3c0719fe6cb76d4
ananyagowdakn <ananyagowdakn@gmail.com>
2025-11-19 08:12:26 +0530

    Add program to check if a number is positive

---
+++ p4a.c
@@ -0,0 +1,14 @@
+//c program to check positive number
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n > 0)
+        printf("Positive");
+    else
+        printf("Not Positive");
+
+    return 0;
+}
